2fa93bd0c734214d22d417102677b085ade51c78
OscarDev-JS <oscargamer162004@gmail.com>
2023-09-11 19:41:35 +0200

    db

--- veterinaria/database.cs
+++ veterinaria/database.cs
@@ -61,8 +61,8 @@ namespace veterinaria
     class Usuario
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string Password { get; set; }
-        public string DisplayName { get; set; }
+        public string Name { get; set; } = "";
+        public string Password { get; set; } = "";
+        public string DisplayName { get; set; } = "";
     }
 }
